29e1630b8ca644eabcc3df598f0dc053bd382205
Nicolas Fargère <nicolas.fargere@gmail.com>
2020-04-10 14:04:50 +0200

    Handle SMTP using SSL

--- src/mail/simpleEmail.py
+++ src/mail/simpleEmail.py
@@ -6,8 +6,12 @@ class simpleEmailHelper(emailHelper):
 		super().__init__(config)		
 		self.smtpServer = config['SMTP']['Server']
 		self.smtpPort = config['SMTP']['Port']
+		self.username = config['SMTP']['username']
+		self.password = config['SMTP']['password']
 		
-	def sendEmail(self, message):		
-		s = smtplib.SMTP(self.smtpServer, self.smtpPort)		
+	def sendEmail(self, message):	
+		s = smtplib.SMTP_SSL(self.smtpServer, self.smtpPort)
+		s.ehlo()
+		s.login(self.username, self.password)		
 		s.sendmail(self.emailFrom, self.emailTo, message.as_string())
-		s.quit()
+		s.close()
